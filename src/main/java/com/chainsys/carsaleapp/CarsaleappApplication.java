940cf687e234c5e3cb758fe3c1b88dc7090bb3d6
ChainsysTraining <naresh.20191224@gmail.com>
2020-02-22 05:57:05 +0530

    Update CarsaleappApplication.java

--- src/main/java/com/chainsys/carsaleapp/CarsaleappApplication.java
+++ src/main/java/com/chainsys/carsaleapp/CarsaleappApplication.java
@@ -3,12 +3,18 @@ package com.chainsys.carsaleapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
 @ServletComponentScan("com.chainsys.carsale.servlet")
-public class CarsaleappApplication {
-
+public class CarsaleappApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	   return application.sources(CarsaleappApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CarsaleappApplication.class, args);
 	}
